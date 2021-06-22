a4088a99fc305c71e19c7fe1b036e659f14cac07
Rinat <rinat@kloop.kg>
2021-06-22 13:39:47 +0600

    Add admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Campaign, Chain, \
+    TaskStage, WebHookStage, ConditionalStage, Case, Task
 
-# Register your models here.
+admin.site.register(Campaign)
+admin.site.register(Chain)
+admin.site.register(TaskStage)
+admin.site.register(WebHookStage)
+admin.site.register(ConditionalStage)
+admin.site.register(Case)
+admin.site.register(Task)
