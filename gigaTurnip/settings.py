eb8412d98e65b75d12c61a95cfde9f37e76fb45c
xakim <xakim@pop-os.localdomain>
2021-07-06 16:06:34 +0600

    Modify settings for App Engine Deploy

--- gigaTurnip/settings.py
+++ gigaTurnip/settings.py
@@ -85,12 +85,19 @@ WSGI_APPLICATION = 'gigaTurnip.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.getenv("APP_ENGINE_DEPLOY", None):
+    DATABASES = {"default": ast.literal_eval(os.getenv('DB'))}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+
+if os.getenv("USE_CLOUD_SQL_AUTH_PROXY", None):
+    DATABASES["default"]["HOST"] = "127.0.0.1"
+    DATABASES["default"]["PORT"] = 3306
 
 
 # Password validation
@@ -130,6 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
