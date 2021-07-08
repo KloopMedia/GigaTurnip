a02acd2efeeffc8882c34f8d263a2a298c560f15
xakim <xakim@pop-os.localdomain>
2021-07-08 16:17:47 +0600

    Modify CORS_ORIGIN_WHITELIST

--- gigaTurnip/settings.py
+++ gigaTurnip/settings.py
@@ -147,7 +147,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'https://kloopmedia.github.io'
 ]
 
 REST_FRAMEWORK = {
