02527ff22c9afc20a932dfda730921249f828d76
xakim <xakim@pop-os.localdomain>
2021-07-09 23:03:46 +0600

    Fix somebody's strange commit ((

--- api/permissions.py
+++ api/permissions.py
@@ -1,50 +1,5 @@
 from rest_access_policy import AccessPolicy
 
-class CampaignAccessPolicy(AccessPolicy):
-    statements = [
-        {
-            "action": ["list"],
-            "principal": "authenticated",
-            "effect": "allow"
-        },
-        {
-            "action": ["create"],
-            "principal": ["group:campaign_creator"],
-            "effect": "allow"
-        },
-        {
-            "action": ["destroy"],
-            "principal": ["group:campaign_creator"],
-            "effect": "allow"
-        },
-        {
-            "action": ["partial_update"],
-            "principal": ["*"],
-            "effect": "allow",
-            "condition": "is_manager"
-        }
-    ]
-
-    def is_manager(self, request, view, action) -> bool:
-        campaign = view.get_object()
-        managers = campaign.managers.all()
-
-        return request.user in managers
-
-class ChainAccessPolicy(AccessPolicy):
-    statements = [
-        {
-            "action": ["list"],
-            "principal": "authenticated",
-            "effect": "allow"
-        },
-        {
-            "action": ["create"],
-            "principal": ["group:campaign_creator"],
-            "effect": "allow"
-        }
-    ]
-from rest_access_policy import AccessPolicy
 
 class CampaignAccessPolicy(AccessPolicy):
     statements = [
@@ -77,6 +32,7 @@ class CampaignAccessPolicy(AccessPolicy):
 
         return request.user in managers
 
+
 class ChainAccessPolicy(AccessPolicy):
     statements = [
         {
@@ -90,6 +46,8 @@ class ChainAccessPolicy(AccessPolicy):
             "effect": "allow"
         }
     ]
+
+
 class TaskAccessPolicy(AccessPolicy):
     statements = [
         {
