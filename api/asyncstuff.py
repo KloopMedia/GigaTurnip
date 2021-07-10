6e004cacdc16bedc687f54daae9ab197560d0990
genvarek <genvarek@gmail.com>
2021-07-10 14:41:48 +0600

    Add responses checking by conditions

--- api/asyncstuff.py
+++ api/asyncstuff.py
@@ -1,7 +1,8 @@
+import json
+import re
 from django.db.models import Q
 
 from api.models import Case, Stage, TaskStage, ConditionalStage, Task
-
 
 
 def process_completed_task(task):
@@ -64,6 +65,46 @@ def process_conditional(stage, in_task):
 
 
 def evaluate_conditional_stage(stage, task):
-    return False
+    """Checks each response
+       Returns True if all responses exist and fit to the conditions
+    """
+    rules = stage.condition
+    responses = task.responses
+    results = list()
+    for rule in rules["logic_array"]:
+
+        control_value = rule["value"]
+        condition = rule["condition"]
+        actual_value = get_value_from_dotted(rule["field"], responses)
+
+        if condition == "==":
+            results.append(control_value == actual_value)
+        elif condition == "!=":
+            results.append(control_value != actual_value)
+        elif condition == ">":
+            results.append(control_value > actual_value)
+        elif condition == "<":
+            results.append(control_value < actual_value)
+        elif condition == ">=":
+            results.append(control_value >= actual_value)
+        elif condition == "<=":
+            results.append(control_value <= actual_value)
+        elif condition == "ARRAY-CONTAINS":
+            results.append(control_value in actual_value)
+        elif condition == "ARRAY-CONTAINS-NOT":
+            results.append(control_value not in actual_value)
+
+    return all(results)
 
 
+def get_value_from_dotted(dotted_path, source_dict):
+    """Turns dotted_path into regular dict keys and returns the value.
+    """
+    fields = dotted_path.split(".")
+    result = source_dict
+    for field in fields:
+        try:
+            result = result[field]
+        except KeyError:
+            return None
+    return result
