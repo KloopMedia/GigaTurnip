1f4dc779341d4a390c8a0ba1c84c29bfd60a7169
artur <arturkabirov16@gmail.com>
2021-07-21 12:50:56 +0600

    Start writing tests for models and views

---
+++ api/tests/test_models.py
@@ -0,0 +1,70 @@
+from django.test import TestCase
+from api.models import CustomUser, BaseModel, SchemaProvider, Campaign, \
+	CampaignManagement, Chain, Stage, TaskStage, \
+	WebHookStage, ConditionalStage, Case, Task, \
+	Rank, Track, RankRecord, RankLimit
+
+
+class CustomUserModelTest(TestCase):
+	pass
+
+
+class BaseModelModelTest(TestCase):
+	pass
+
+
+class SchemaProviderModelTest(TestCase):
+	pass
+
+
+class CampaignModelTest(TestCase):
+	pass
+
+
+class CampaignManagementModelTest(TestCase):
+	pass
+
+
+class ChainModelTest(TestCase):
+	pass
+
+
+class StageModelTest(TestCase):
+	pass
+
+
+class TaskStageModelTest(TestCase):
+	pass
+
+
+class WebHookStageModelTest(TestCase):
+	pass
+
+
+class ConditionalStageModelTest(TestCase):
+	pass
+
+
+class CaseModelTest(TestCase):
+	pass
+
+
+class TaskModelTest(TestCase):
+	pass
+
+
+class RankModelTest(TestCase):
+	pass
+
+
+class TrackModelTest(TestCase):
+	pass
+
+
+class RankRecordModelTest(TestCase):
+	pass
+
+
+class RankLimitModelTest(TestCase):
+	pass
+
