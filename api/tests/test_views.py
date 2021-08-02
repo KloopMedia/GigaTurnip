00cd4b811df060b7bdc53a843bb6458c60e63699
artur <arturkabirov16@gmail.com>
2021-08-02 13:57:09 +0600

    Add tests for TaskStages viewset (test user relevant method)

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -1,16 +1,17 @@
-import django
+import django, json
 
 django.setup()
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase, APIRequestFactory, force_authenticate
 from django.contrib.auth.models import Group
-from api.models import CustomUser, Campaign
+from api.models import CustomUser, Campaign, TaskStage, Task, Chain, Rank, RankLimit, RankRecord
 from django.db.models import Q
+from rest_framework import status
+from api.views import CampaignViewSet, TaskStageViewSet, TaskViewSet
+from api.serializer import TaskStageSerializer, TaskStageReadSerializer
 
-from api.views import CampaignViewSet
 
-
-class CampaignTest(APITestCase):
+class CampaignViewSetTest(APITestCase):
 	def setUp(self):
 		self.url = reverse('campaign-list')
 		self.factory = APIRequestFactory()
@@ -25,18 +26,18 @@ class CampaignTest(APITestCase):
 
 	def test_get_list_of_campaigns_if_user_is_not_manager_of_any_campaign(self):
 		response = self.client.get(self.url)
-		self.assertEqual(response.status_code, 404)
+		self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
 	def test_get_list_of_campaigns_if_user_not_managers_of_any_campaign(self):
 		self.user.managed_campaigns.add(self.campaign)
 		response = self.client.get(self.url)
-		self.assertEqual(response.status_code, 200)
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 	def test_create_new_campaign_if_not_campaign_creator(self):
 		request = self.factory.post(self.url, {"name": "created in test"})
 		force_authenticate(request=request, user=self.user)
 		response = self.view(request)
-		self.assertEqual(response.status_code, 403)
+		self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
 	def test_create_new_campaign_if_campaign_creator(self):
 		self.user.groups.add(self.campaign_creator)
@@ -49,13 +50,13 @@ class CampaignTest(APITestCase):
 		self.user.managed_campaigns.add(self.campaign)
 		not_my_campaign = Campaign.objects.filter(~Q(id=self.campaign.id))[0]
 		response = self.client.get(self.url + str(not_my_campaign.id) + '/')
-		self.assertEqual(response.status_code, 403)
+		self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
 	def test_retrieve_my_campaign(self):
 		self.user.managed_campaigns.add(self.campaign)
 		my_campaign = self.user.managed_campaigns.first()
 		response = self.client.get(self.url + str(my_campaign.id) + '/')
-		self.assertEqual(response.status_code, 200)
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 	def test_partial_update_not_my_campaign(self):
 		not_my_campaign = Campaign.objects.first()
@@ -63,7 +64,7 @@ class CampaignTest(APITestCase):
 		request = self.factory.patch(url, {"name": "renamed campaign in test without permissions"})
 		force_authenticate(request=request, user=self.user)
 		response = self.view(request, pk=str(not_my_campaign.id))
-		self.assertEqual(response.status_code, 403)
+		self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
 	def test_partial_update_my_campaign(self):
 		self.user.managed_campaigns.add(self.campaign)
@@ -72,4 +73,65 @@ class CampaignTest(APITestCase):
 		request = self.factory.patch(url, {"name": "renamed campaign in test had permissions"})
 		force_authenticate(request=request, user=self.user)
 		response = self.view(request, pk=str(my_campaign.id))
-		self.assertEqual(response.status_code, 200)
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class TaskStageViewSetTest(APITestCase):
+	def setUp(self):
+		self.url = reverse('taskstage-list')
+		self.factory = APIRequestFactory()
+		self.user = CustomUser.objects.create_user(
+			username='test',
+			email='test@email.com',
+			password='test')
+
+		self.client.force_authenticate(user=self.user)
+
+	def test_user_relevant_if_had_task_stage_and_rank(self):
+		campaign = Campaign.objects.create(name='Testing campaign views')
+		chain = Chain.objects.create(name='Testing chain views', campaign=campaign)
+		task_stage = TaskStage.objects.create(
+			name='Task stage testing', chain=chain,
+			x_pos=1, y_pos=1, is_creatable=True)
+		rank = Rank.objects.create(name='Testing rank views')
+		rank_record = RankRecord.objects.create(user=self.user, rank=rank)
+		rank_limit = RankLimit.objects.create(
+			rank=rank, stage=task_stage,
+			open_limit=3, total_limit=5
+		)
+		response = self.client.get(self.url + 'user_relevant/')
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+	def test_many_ranks_and_task_stages(self):
+		campaign = Campaign.objects.create(name='Testing campaign views')
+		chain = Chain.objects.create(name='Testing chain views', campaign=campaign)
+
+		task_stages = [TaskStage.objects.create(
+			name=f'Task stage testing #{i}', chain=chain,
+			x_pos=1, y_pos=1, is_creatable=True) for i in range(3)]
+
+		ranks = [Rank.objects.create(name=f'Testing rank views №{i}') for i in range(3)]
+		rank_records = [RankRecord.objects.create(user=self.user, rank=rank) for rank in ranks]
+		ranks_and_task_stages = zip(ranks, task_stages)
+		rank_limits = [RankLimit.objects.create(
+			rank=rank_and_stage[0], stage=rank_and_stage[1],
+			open_limit=2+i, total_limit=2+i
+		) for i, rank_and_stage in enumerate(ranks_and_task_stages)]
+
+		for i, rank_limit in enumerate(rank_limits):
+			open_limit = rank_limit.open_limit
+			total_limit = rank_limit.total_limit
+
+			if total_limit < 4:
+				tasks_for_current_task_stage = [
+					Task.objects.create(
+						assignee=self.user,
+						stage=task_stages[i]
+					) for j in range(total_limit)
+				]
+
+		response = self.client.get(self.url + 'user_relevant/', format='json')
+		expected_instance = TaskStageReadSerializer(task_stages[-1]).data
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
+		self.assertEqual(len(response.data), 1)
+		self.assertIn(expected_instance, json.loads(response.content))
