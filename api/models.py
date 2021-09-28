b904d7d2cd77b6169040cc592c6db5814e3e365f
Rinat <rinat@kloop.kg>
2021-09-28 12:13:11 +0600

    Decrease line length in models.py

--- api/models.py
+++ api/models.py
@@ -12,17 +12,30 @@ class CustomUser(AbstractUser):
 
 
 class BaseModel(models.Model):
-    name = models.CharField(max_length=100, help_text="Instance name")
-    description = models.TextField(blank=True, help_text="Instance description")
+    name = models.CharField(max_length=100,
+                            help_text="Instance name")
+    description = models.TextField(blank=True,
+                                   help_text="Instance description")
 
     class Meta:
         abstract = True
 
 
 class SchemaProvider(models.Model):
-    json_schema = models.TextField(null=True, blank=True, help_text="Defines the underlying data to be shown in the UI (objects, properties, and their types)")
-    ui_schema = models.TextField(null=True, blank=True, help_text="Defines how JSON data is rendered as a form, e.g. the order of controls, their visibility, and the layout")
-    library = models.CharField(max_length=200, blank=True, help_text="Type of JSON form library")
+    json_schema = models.TextField(null=True,
+                                   blank=True,
+                                   help_text="Defines the underlying data "
+                                             "to be shown in the UI (objects, "
+                                             "properties, and their types)")
+    ui_schema = models.TextField(null=True,
+                                 blank=True,
+                                 help_text="Defines how JSON data is rendered "
+                                           "as a form, e.g. the order of "
+                                           "controls, their visibility, "
+                                           "and the layout")
+    library = models.CharField(max_length=200,
+                               blank=True,
+                               help_text="Type of JSON form library")
 
     class Meta:
         abstract = True
@@ -70,8 +83,16 @@ class Chain(BaseModel):
 
 
 class Stage(PolymorphicModel, BaseModel):
-    x_pos = models.DecimalField(max_digits=17, decimal_places=14, help_text="Starting position of 'x' coordinate to draw on Giga Turnip Chain frontend interface")
-    y_pos = models.DecimalField(max_digits=17, decimal_places=14, help_text="Starting position of 'y' coordinate to draw on Giga Turnip Chain frontend interface")
+    x_pos = models.DecimalField(max_digits=17,
+                                decimal_places=14,
+                                help_text="Starting position of 'x' "
+                                          "coordinate to draw on Giga Turnip "
+                                          "Chain frontend interface")
+    y_pos = models.DecimalField(max_digits=17,
+                                decimal_places=14,
+                                help_text="Starting position of 'y' "
+                                          "coordinate to draw on Giga Turnip "
+                                          "Chain frontend interface")
     chain = models.ForeignKey(Chain,
                               on_delete=models.CASCADE,
                               related_name="stages",
@@ -90,15 +111,25 @@ class Stage(PolymorphicModel, BaseModel):
 
 
 class TaskStage(Stage, SchemaProvider):
-    rich_text = models.TextField(null=True, blank=True,
-                                 help_text="Text field with rich HTML formatting, can be used for manuals")
+    rich_text = models.TextField(null=True,
+                                 blank=True,
+                                 help_text="Text field with rich HTML "
+                                           "formatting, can be used "
+                                           "for manuals")
     copy_input = models.BooleanField(default=False, help_text="")
-    allow_multiple_files = models.BooleanField(default=False, help_text="Allow user to upload multiple files")
-    is_creatable = models.BooleanField(default=False, help_text="Allow user to create a task manually")
+    allow_multiple_files = models.BooleanField(default=False,
+                                               help_text="Allow user to upload"
+                                                         " multiple files")
+    is_creatable = models.BooleanField(default=False,
+                                       help_text="Allow user to create a task"
+                                                 " manually")
     displayed_prev_stages = models.ManyToManyField(Stage,
                                                    related_name="displayed_following_stages",
                                                    blank=True,
-                                                   help_text="List of previous stages (tasks data) to be shown in current stage")
+                                                   help_text="List of previous"
+                                                             " stages (tasks "
+                                                             "data) to be shown"
+                                                             " in current stage")
 
     RANK = 'RA'
     STAGE = 'ST'
@@ -109,14 +140,18 @@ class TaskStage(Stage, SchemaProvider):
     assign_user_by = models.CharField(max_length=2,
                                       choices=ASSIGN_BY_CHOICES,
                                       default=RANK,
-                                      help_text="User assignment method (by 'Stage' or by 'Rank')")
+                                      help_text="User assignment method "
+                                                "(by 'Stage' or by 'Rank')")
 
     assign_user_from_stage = models.ForeignKey(Stage,
                                                on_delete=models.CASCADE,
                                                related_name="assign_user_to_stages",
                                                blank=True,
                                                null=True,
-                                               help_text="Stage id. User from assign_user_from_stage will be assigned to a task")
+                                               help_text="Stage id. User from"
+                                                         " assign_user_from_stage "
+                                                         "will be assigned "
+                                                         "to a task")
 
 
 class WebHookStage(Stage, SchemaProvider):
@@ -128,8 +163,11 @@ class WebHookStage(Stage, SchemaProvider):
 
 
 class ConditionalStage(Stage):
-    conditions = models.JSONField(null=True, help_text="JSON logic conditions")
-    pingpong = models.BooleanField(default=False, help_text="If True, makes 'in stages' task incomplete")
+    conditions = models.JSONField(null=True,
+                                  help_text="JSON logic conditions")
+    pingpong = models.BooleanField(default=False,
+                                   help_text="If True, makes 'in stages' "
+                                             "task incomplete")
 
     # def __str__(self):
     #     return str("Conditional Stage Filler for " + self.stage__str__())
@@ -148,7 +186,8 @@ class Task(models.Model):
                                  related_name="tasks",
                                  blank=True,
                                  null=True,
-                                 help_text="User id who is responsible for the task")
+                                 help_text="User id who is responsible "
+                                           "for the task")
     stage = models.ForeignKey(TaskStage,
                               on_delete=models.CASCADE,
                               related_name="tasks",
@@ -159,7 +198,10 @@ class Task(models.Model):
                              blank=True,
                              null=True,
                              help_text="Case id")
-    responses = models.JSONField(null=True, blank=True, help_text="User generated responses (answers)")
+    responses = models.JSONField(null=True,
+                                 blank=True,
+                                 help_text="User generated responses "
+                                           "(answers)")
     in_tasks = models.ManyToManyField("self",
                                       related_name="out_tasks",
                                       blank=True,
@@ -185,7 +227,9 @@ class Track(BaseModel):
                                  related_name="tracks",
                                  on_delete=models.CASCADE,
                                  help_text="Campaign id")
-    ranks = models.ManyToManyField(Rank, related_name="ranks", help_text="Ranks id")
+    ranks = models.ManyToManyField(Rank,
+                                   related_name="ranks",
+                                   help_text="Ranks id")
     default_rank = models.ForeignKey(Rank,
                                      on_delete=models.CASCADE,
                                      blank=True,
@@ -194,8 +238,12 @@ class Track(BaseModel):
 
 
 class RankRecord(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE,  help_text="User id")
-    rank = models.ForeignKey(Rank, on_delete=models.CASCADE,  help_text="Rank id")
+    user = models.ForeignKey(CustomUser,
+                             on_delete=models.CASCADE,
+                             help_text="User id")
+    rank = models.ForeignKey(Rank,
+                             on_delete=models.CASCADE,
+                             help_text="Rank id")
 
     class Meta:
         unique_together = ['user', 'rank']
@@ -205,20 +253,39 @@ class RankRecord(models.Model):
 
 
 class RankLimit(models.Model):
-    rank = models.ForeignKey(Rank, on_delete=models.CASCADE, help_text="Rank id")
+    rank = models.ForeignKey(Rank,
+                             on_delete=models.CASCADE,
+                             help_text="Rank id")
     stage = models.ForeignKey(TaskStage,
                               on_delete=models.CASCADE,
                               related_name="ranklimits",
                               help_text="Stage id")
-    open_limit = models.IntegerField(default=0, help_text="The maximum number of tasks that can be opened at the same time for a user")
-    total_limit = models.IntegerField(default=0, help_text="The maximum number of tasks that user can obtain")
-    is_listing_allowed = models.BooleanField(default=False, help_text="Allow user to see the list of created tasks")
-    is_submission_open = models.BooleanField(default=True, help_text="Allow user to submit a task")
-    is_selection_open = models.BooleanField(default=True, help_text="Allow user to select a task")
-    is_creation_open = models.BooleanField(default=True, help_text="Allow user to create a task")
+    open_limit = models.IntegerField(default=0,
+                                     help_text="The maximum number of tasks "
+                                               "that can be opened at the same"
+                                               " time for a user")
+    total_limit = models.IntegerField(default=0,
+                                      help_text="The maximum number of tasks "
+                                                "that user can obtain")
+    is_listing_allowed = models.BooleanField(default=False,
+                                             help_text="Allow user to see "
+                                                       "the list of created "
+                                                       "tasks")
+    is_submission_open = models.BooleanField(default=True,
+                                             help_text="Allow user to submit "
+                                                       "a task")
+    is_selection_open = models.BooleanField(default=True,
+                                            help_text="Allow user to select"
+                                                      " a task")
+    is_creation_open = models.BooleanField(default=True,
+                                           help_text="Allow user to create"
+                                                     " a task")
 
     class Meta:
         unique_together = ['rank', 'stage']
 
     def __str__(self):
-        return str("Rank limit: " + self.rank.__str__() + " " + self.stage.__str__())
+        return str("Rank limit: " +
+                   self.rank.__str__() +
+                   " " +
+                   self.stage.__str__())
