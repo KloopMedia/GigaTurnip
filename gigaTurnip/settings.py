15aac54485821ed4fe0a6edccad31a7c5ae93599
xakim1c <khakim.davurov@gmail.com>
2021-10-14 19:05:42 +0600

    Modify DEBUG mode

--- gigaTurnip/settings.py
+++ gigaTurnip/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7n1&zuwh^_mk==hho2aj3x%hy@vh!681-e_-@=_@et^lz^x=_)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
