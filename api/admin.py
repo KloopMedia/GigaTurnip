88ab7fbc7183e0d826800d3de2da22fc15b8ee9a
Rinat <rinat@kloop.kg>
2021-10-15 18:09:04 +0600

    Improve admin interface

--- api/admin.py
+++ api/admin.py
@@ -9,13 +9,39 @@ class CustomUserAdmin(UserAdmin):
     model = CustomUser
 
 
+class ChainAdmin(admin.ModelAdmin):
+    list_display = ('name', 'campaign', )
+    list_filter = ('campaign',)
+    search_fields = ('name',)
+
+
+class StageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'chain',)
+    list_filter = ('chain__campaign', 'chain')
+    search_fields = ('name',
+                     'chain__name',
+                     'chain__campaign__name', )
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'stage', 'assignee', )
+    list_filter = ('stage__chain__campaign',
+                   'stage__chain',
+                   'stage', )
+    search_fields = ('id',
+                     'stage__name',
+                     'assignee__email',
+                     'stage__chain__name',
+                     'stage__chain__campaign__name')
+
+
 admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Campaign)
-admin.site.register(Chain)
-admin.site.register(TaskStage)
-admin.site.register(ConditionalStage)
+admin.site.register(Chain, ChainAdmin)
+admin.site.register(TaskStage, StageAdmin)
+admin.site.register(ConditionalStage, StageAdmin)
 admin.site.register(Case)
-admin.site.register(Task)
+admin.site.register(Task, TaskAdmin)
 admin.site.register(Rank)
 admin.site.register(RankLimit)
 admin.site.register(RankRecord)
