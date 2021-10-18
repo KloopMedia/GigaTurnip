35d4638b5cd4f6d8363948f7c35aea4a5d05aa5a
Rinat <rinat@kloop.kg>
2021-10-18 13:07:39 +0600

    Add empty responses Tasks Admin filter

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,35 @@
 from django.contrib import admin
+from django.contrib.admin import SimpleListFilter
 from django.contrib.auth.admin import UserAdmin
 
 from .models import Campaign, Chain, \
     TaskStage, ConditionalStage, Case, Task, CustomUser, Rank, RankLimit, RankRecord, CampaignManagement, Track
+
+
+class TaskResponsesStatusFilter(SimpleListFilter):
+    title = "Responses JSON Status"
+    parameter_name = "Responses"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("not_empty", "Filled Responses"),
+            ("json_empty", "Empty JSON Responses ({})"),
+            ("empty_string", "Empty String Responses()"),
+            ("null", "Null Responses (__isnull==True)")
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == "json_empty":
+            return queryset.distinct().filter(responses__iexact="{}")
+        elif self.value() == "null":
+            return queryset.distinct().filter(responses__isnull=True)
+        elif self.value() == "empty_string":
+            return queryset.distinct().filter(responses__iexact="")
+        elif self.value() == "not_empty":
+            return queryset.distinct()\
+                .exclude(responses__iexact="")\
+                .exclude(responses__iexact="{}")\
+                .exclude(responses__isnull=True)
 
 
 class CustomUserAdmin(UserAdmin):
@@ -35,7 +62,8 @@ class TaskAdmin(admin.ModelAdmin):
     list_filter = ('stage__chain__campaign',
                    'stage__chain',
                    'stage',
-                   'complete', )
+                   'complete',
+                   TaskResponsesStatusFilter, )
     search_fields = ('id',
                      'case__id',
                      'stage__name',
