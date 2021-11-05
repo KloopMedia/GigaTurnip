40ada2e9ecfe79c7791569430d12de8c780b407e
artur <arturkabirov16@gmail.com>
2021-11-05 14:49:03 +0300

    Add tests for admin actions mark complete in Tasks

--- api/tests/test_admin_actions.py
+++ api/tests/test_admin_actions.py
@@ -5,15 +5,16 @@ from api.models import CustomUser, Campaign, Chain, ConditionalStage, TaskStage,
 
 class TaskAdminTest(TestCase):
     def setUp(self):
-        self.url_tasks = reverse('task-list')
+        self.url = reverse("admin:api_task_changelist")
 
         self.u_password = '123'
-        self.user = CustomUser.objects.create_superuser(username="test", email='test@email.com', password=self.u_password)
+        self.user = CustomUser.objects.create_superuser(username="test", email='test@email.com',
+                                                        password=self.u_password)
+        self.user.is_staff = True
+        self.user.save()
         self.new_user = CustomUser.objects.create_user(username="new_user", email='new_user@email.com',
                                                        password='new_user')
         self.employee = CustomUser.objects.create(username="empl", email='empl@email.com', password='empl')
-
-        # self.client.force_authenticate(user=self.user)
 
         self.campaign = Campaign.objects.create(name="Campaign")
         self.chain = Chain.objects.create(name="Chain", campaign=self.campaign)
@@ -25,17 +26,28 @@ class TaskAdminTest(TestCase):
         self.another_chain = Chain.objects.create(name="Chain", campaign=self.another_campaign)
         self.another_task_stage = TaskStage.objects.create(name="Task stage", x_pos=1, y_pos=1,
                                                            chain=self.another_chain)
-
     def test_mark_complete(self):
-        self.user.is_staff = True
-        self.user.save()
+        case = Case.objects.create()
+        task = Task.objects.create(assignee=self.new_user, stage=self.task_stage, case=case)
+        new_task_stage = TaskStage.objects.create(name="Task stage", x_pos=1, y_pos=1,
+                                                  chain=self.chain)
+        new_task_stage.in_stages.add(self.task_stage)
+
+        data = {'action': 'make_completed',
+                '_selected_action': [task.id]}
+
+        self.client.login(username=self.user.username, password=self.u_password)
+        self.client.post(self.url, data, follow=True)
+        self.assertTrue(Task.objects.get(id=task.id).complete)
+
+    def test_mark_complete_force(self):
         case = Case.objects.create()
         tasks = [Task.objects.create(assignee=self.new_user, stage=self.task_stage, case=case,
-                                    complete=False) for x in range(5)]
+                                     complete=False) for x in range(5)]
         tasks_complete = tasks[:3]
         data = {'action': 'make_completed_force',
                 '_selected_action': [t.id for t in tasks_complete]}
-        change_url = reverse("admin:api_task_changelist")
         self.client.login(username=self.user.username, password=self.u_password)
-        response = self.client.post(change_url, data, follow=True)
-        self.assertEqual(list(Task.objects.filter(complete=True)), list(Task.objects.filter(pk__in=data['_selected_action'])))
+        response = self.client.post(self.url, data, follow=True)
+        self.assertEqual(list(Task.objects.filter(complete=True)),
+                         list(Task.objects.filter(pk__in=data['_selected_action'])))
