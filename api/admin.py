b71a6f34bd7a7184e256203f6e87a37fe689bce5
Rinat <rinat@kloop.kg>
2021-11-26 09:56:20 +0600

    Fix stage deletion

--- api/admin.py
+++ api/admin.py
@@ -182,8 +182,8 @@ class GeneralStageAdmin(admin.ModelAdmin):
     def has_view_or_change_permission(self, request, obj=None):
         return False
 
-    def has_delete_permission(self, request, obj=None):
-        return False
+    # def has_delete_permission(self, request, obj=None):
+    #     return False
 
     def has_module_permission(self, request):
         return False
