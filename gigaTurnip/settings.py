e74efcb4576e093296681b62315fc53b5b8c8ef2
artur <arturkabirov16@gmail.com>
2022-04-25 15:42:09 +0300

    Fix bug with token basic auth.

--- gigaTurnip/settings.py
+++ gigaTurnip/settings.py
@@ -159,7 +159,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
         'drf_firebase_auth.authentication.FirebaseAuthentication',
-        'rest_framework.authentication.BasicAuthentication'
+        'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
