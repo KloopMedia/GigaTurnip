f1ac45d9a583b4ecaabbe0befa4446c79e951ad6
artur <arturkabirov16@gmail.com>
2022-11-01 18:28:21 +0300

    Add select_related in Task scoping queryset.

--- api/permissions.py
+++ api/permissions.py
@@ -298,7 +298,7 @@ class TaskAccessPolicy(AccessPolicy):
 
     @classmethod
     def scope_queryset(cls, request, queryset):
-        return queryset. \
+        return queryset.prefetch_related('stage', 'stage__chain'). \
             filter(stage__chain__campaign__campaign_managements__user=
                    request.user).distinct()
 
