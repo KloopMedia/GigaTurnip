6aeb56375dd158baffb6c906454a3a2332c901de
karim3487 <karim3487@gmail.com>
2023-01-10 21:11:09 +0300

    Add TestWebhook model.

--- api/models.py
+++ api/models.py
@@ -652,6 +652,22 @@ class Webhook(BaseDatesModel):
         return response
 
 
+class TestWebhook(BaseDatesModel):
+    expected_task = models.OneToOneField(
+        'Task',
+        related_name='expected_task',
+        blank=False,
+        help_text='task, with the answers you expect to answer',
+        on_delete=models.CASCADE
+    )
+    sent_task = models.OneToOneField(
+        'Task',
+        related_name='sent_task',
+        blank=False,
+        help_text='task to be sent',
+        on_delete=models.CASCADE
+    )
+
 class DatetimeSort(BaseDatesModel):
     stage = models.OneToOneField(
         Stage,
