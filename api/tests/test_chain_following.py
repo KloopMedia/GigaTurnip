174d4fc0d9be8399963dcc23f01b17463f8c0368
artur <arturkabirov16@gmail.com>
2023-02-20 11:40:38 +0300

    Paginate list endpoints.

--- api/tests/test_chain_following.py
+++ api/tests/test_chain_following.py
@@ -2182,7 +2182,6 @@ class GigaTurnipTest(APITestCase):
         content = json.loads(response.content)
         self.assertEqual(len(content['results']), 0)
 
-
     def test_task_with_timer_is_exist(self):
         second_stage = self.initial_stage.add_stage(TaskStage(
             assign_user_by="RA"
@@ -2212,7 +2211,7 @@ class GigaTurnipTest(APITestCase):
 
         response = self.get_objects('task-user-relevant')
         content = json.loads(response.content)
-        self.assertEqual(len(content), 1)
+        self.assertEqual(len(content['results']), 1)
 
     def test_test_webhook(self):
         task = self.create_initial_task()
