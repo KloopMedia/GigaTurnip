c556d712f2cd459bd729fcc3ba3a69db15899ab1
artur <arturkabirov16@gmail.com>
2023-04-13 13:44:38 +0300

    Add Language model.

--- api/models.py
+++ api/models.py
@@ -8,6 +8,7 @@ import requests
 from django.apps import apps
 from django.contrib.auth.models import AbstractUser
 from django.contrib.postgres.fields import ArrayField
+from django.core.exceptions import ValidationError
 from django.core.validators import MaxValueValidator
 from django.db import models, transaction, OperationalError
 from django.db.models import UniqueConstraint, Q
@@ -185,6 +186,34 @@ class Country(models.Model):
 
     def __str__(self):
         return self.name
+
+
+def validate_language_code(val):
+    if len(val) == 2 and val.isalpha():
+        return val.lower()
+    raise ValidationError(
+        "This field is two letters code(format ISO 639-1).\n"
+        "Check available codes in wikipedia: https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes."
+    )
+
+
+class Language(models.Model):
+    name = models.CharField(
+        max_length=128,
+        blank=False,
+        null=False,
+        help_text="ISO language name."
+    )
+    code = models.CharField(
+        max_length=2,
+        blank=False,
+        null=False,
+        validators=[validate_language_code],
+        help_text="Two letters code of language."
+    )
+
+    def __str__(self):
+        return "{}: {}".format(self.name, self.code)
 
 
 class Campaign(BaseModel, CampaignInterface):
