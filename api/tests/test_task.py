e0b5151004177316e12e92f653c722081eb62594
Rinat <rinat@kloop.kg>
2023-07-05 12:53:22 +0600

    Refactor tests

--- api/tests/test_task.py
+++ api/tests/test_task.py
@@ -128,40 +128,6 @@ class TaskTest(GigaTurnipTestHelper):
         self.assertEqual(len(response.data["results"]), 1)
         self.assertEqual(response.data["results"][0]["id"], task_2.id)
 
-    def test_task_stage_schema(self):
-        stage_schema = {
-            "type": "object",
-            "properties": {
-                "answer": {"type": "string"}
-            },
-            "required": ["answer"]
-        }
-        stage_ui_schema = {"ui:order": ["answer"]}
-        task_schema = {
-            "type": "object",
-            "properties": {
-                "answer_to_generated_question": {"type": "string"}
-            },
-            "required": ["answer"]
-        }
-        task_ui_schema = {"ui:order": ["answer_to_generated_question"]}
-
-        self.initial_stage.json_schema = json.dumps(stage_schema)
-        self.initial_stage.ui_schema = json.dumps(stage_ui_schema)
-        self.initial_stage.save()
-
-        task = self.create_initial_task()
-        task.schema = json.dumps(task_schema)
-        task.ui_schema = json.dumps(task_ui_schema)
-        task.save()
-
-        response = self.get_objects("task-detail", pk=task.pk)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data["stage"]["json_schema"],
-                         json.dumps(stage_schema))
-        self.assertEqual(response.data["stage"]["ui_schema"],
-                         json.dumps(stage_ui_schema))
-
     def test_get_user_activity_csv_fail(self):
         self.create_initial_tasks(5)
         response = self.client.get(reverse('task-user-activity-csv') + "?csv=22")
