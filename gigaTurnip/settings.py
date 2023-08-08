1ce90bc7b4f5718fbfefe0bdd7ed762707527efd
xakim1c <khakim.davurov@gmail.com>
2023-08-08 22:41:09 +0600

    Fix CORS bug

--- gigaTurnip/settings.py
+++ gigaTurnip/settings.py
@@ -189,7 +189,7 @@ CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
     'https://kloopmedia.github.io',
     'https://talaybekovna.github.io',
-    'https://atna-lab.github.io/'
+    'https://atna-lab.github.io'
 ]
 
 REST_FRAMEWORK = {
