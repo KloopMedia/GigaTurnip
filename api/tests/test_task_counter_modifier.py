ab84a3997e4caf878a693284328cd65c83bc0b31
atna <atay.narynov@gmail.com>
2023-10-23 17:51:25 +0600

    Add test for task count modifier with unique users.

--- api/tests/test_task_counter_modifier.py
+++ api/tests/test_task_counter_modifier.py
@@ -26,3 +26,29 @@ class TaskCounterTest(GigaTurnipTestHelper):
         self.assertEqual(task_to_check.responses["task_count"], 3)
 
 
+    def test_task_count_unique_users(self):
+            verification_task_stage = self.initial_stage.add_stage(TaskStage())
+
+            CountTasksModifier.objects.create(
+                task_stage=verification_task_stage,
+                stage_to_count_tasks_from=self.initial_stage,
+                field_to_write_count_to="task_count",
+                count_unique_users=True,
+            )
+
+            task = self.create_task(self.initial_stage)
+
+            self.create_task(self.initial_stage)
+
+            client = self.prepare_client(
+                self.initial_stage,
+                self.employee,
+                RankLimit(is_creation_open=True))
+            self.create_task(self.initial_stage, client=client)
+            
+
+            self.complete_task(task)
+
+            task_to_check = Task.objects.get(case=task.case, stage=verification_task_stage)
+
+            self.assertEqual(task_to_check.responses["task_count"], 2)
