e355d1f4408856a6e413b42b5bd9583fc2b8e90d
xakim1c <khakim.davurov@gmail.com>
2024-01-16 19:07:21 +0600

    Modify CORS

--- gigaTurnip/settings.py
+++ gigaTurnip/settings.py
@@ -185,6 +185,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
     'https://kloopmedia.github.io',
